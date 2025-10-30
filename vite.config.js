9f0737d0b25be426e669b348d7085163b3c46806
Yousef <longliveyousefdalloul@gmail.com>
2025-10-30 10:14:20 +0400

    Fix merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-<<<<<<< HEAD
   esbuild: { jsx: "automatic" },
-=======
->>>>>>> 2e809e278af7df7a7284c2c847d0d0c2b2c9870c
 });
